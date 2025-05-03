2c75eb7c609fa330e7d001e7bde8efc225182c45
SomeoneProb <napieralakamil407@gmail.com>
2025-05-04 01:54:47 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,16 +1,24 @@
-badnames = [
-    "FAG", "fag",
-]
+import requests
+import random
+from flask import Flask, jsonify, request
+import logging
+import json
+
+app = Flask(__name__)
 
 
-@app.route('/api/v1/CheckForBadName', methods=['POST', 'GET'])
-def CheckForBadName():
-    data = request.get_json()
+@app.route("/api/v1/CheckForBadName", methods=["POST"])
+def check_for_bad_name():
+    rjson = request.get_json().get("FunctionResult")
+    name = rjson.get("name").upper()
 
-    name = data.get("FunctionArgument", {}).get("name")
-    forRoom = data.get("FunctionArgument", {}).get("forRoom")
-
-    userid = data.get("CallerEntityProfile",
-                      {}).get("Lineage", {}).get("MasterPlayerAccountId", "None")
-
-    return jsonify({"result": 0})
+    if name in ["KKK", "PENIS", "NIGG", "NEG", "NIGA", "MONKEYSLAVE", "SLAVE", "FAG", 
+        "NAGGI", "TRANNY", "QUEER", "KYS", "DICK", "PUSSY", "VAGINA", "BIGBLACKCOCK", 
+        "DILDO", "HITLER", "KKX", "XKK", "NIGA", "NIGE", "NIG", "NI6", "PORN", 
+        "JEW", "JAXX", "TTTPIG", "SEX", "COCK", "CUM", "FUCK", "PENIS", "DICK", 
+        "ELLIOT", "JMAN", "K9", "NIGGA", "TTTPIG", "NICKER", "NICKA", 
+        "REEL", "NII", "@here", "!", " ", "JMAN", "PPPTIG", "CLEANINGBOT", "JANITOR", "K9", 
+        "H4PKY", "MOSA", "NIGGER", "NIGGA", "IHATENIGGERS", "@everyone", "TTT"]:
+        return jsonify({"result": 2})
+    else:
+        return jsonify({"result": 0})
